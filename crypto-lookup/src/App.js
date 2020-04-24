9f1e1a6ee1124029ef0e883605b8ad7f185da17b
tangela8 <61296102+tangela8@users.noreply.github.com>
2020-04-24 14:16:22 -0400

    added axios

--- crypto-lookup/src/App.js
+++ crypto-lookup/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import './App.css';
-
+import axios from 'axios';
 //have to start with a constructor using state to put the items in an array
-class App extends React.Component {
+class App extends Component {
 
-  constructor() {
-      super();
+  constructor(props) {
+
+      super(props);
 
       this.state = {
-          items: []
+          cryptos: []
       }
 
   }
@@ -17,17 +17,33 @@ class App extends React.Component {
   //use componentdidmount method to fetch the data from the API
   componentDidMount() {
 
-      fetch("https://www.worldcoinindex.com/apiservice/v2getmarkets?key={vjYTfk0YgkCiouojpcQkmOM2CUF0Og}&fiat=btc")
-          .then(results => results.json())
-          .then(json => {
-              this.setState({
-                  items: json,
-                  isLoaded: true, //use isloaded when true
-              })
-          });
+      axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
+      .then(res => {
+        const cryptos = res.data;
+        this.setState({cryptos: cryptos});
+      })
+  }
+
+//next use render to display the data on DOM
+  render() {
+
+      const { cryptos } = this.state;
+
+//use ul to creat a list and map out the items
+      return (
+          <div className="App">
+              <ul>
+                  {cryptos.map(item => (
+                      <li key={cryptos.id}>
+                          Name: {cryptos.name} 
+                      </li>
+                  ))}
+              </ul>
+          </div>
+      );
 
   }
 
-
+}
 
 export default App;
